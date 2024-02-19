d96e1a076b44c77aac3cef458b6bef589b58d242
pbl1234 <136547034+pbl1234@users.noreply.github.com>
2024-02-19 07:56:55 +0700

    Add files via upload

---
+++ Unit_7_Sensor_Kelembaban.ino
@@ -0,0 +1,23 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(13, 12, 11, 10, 9, 8);
+int Sensor = A0;
+
+void setup() {
+  lcd.begin(20, 4);
+  lcd.print("Cek Kelembaban Tanah");
+  lcd.setCursor(0,1);
+  lcd.print("Kelembaban: ");
+  lcd.setCursor(0,2);
+  lcd.print("Volt: ");
+}
+
+void loop() {
+  int kelembaban = analogRead(Sensor);
+  float volt = analogRead(Sensor)*0.0048828125;   
+  lcd.setCursor(14,1); 
+  lcd.print(kelembaban);
+  lcd.setCursor(8, 2); 
+  lcd.print(volt);   
+  lcd.print("V");
+  delay(100);
+}
